cf3c598e398981e3ebc7bcee8b116d51584811ed
Raipagnin <64612453+Raipagnin@users.noreply.github.com>
2020-08-04 12:32:46 +0000

    Update index.js

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -19,7 +19,7 @@ function CadastroCategoria() {
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost')
       ? 'http://localhost:8080/categorias'
-      : 'https://rai-notflix.herokuapp.com/categorias';
+      : 'https://rai-notflix.herokuapp.com//categorias?_embed=videoss';
     // variáveis
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
