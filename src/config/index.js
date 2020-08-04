ac3b28a166c3820ebf3b882a374283aae549998b
Raipagnin <64612453+Raipagnin@users.noreply.github.com>
2020-08-04 12:44:20 +0000

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://rai-notflix.herokuapp.com/';
+  : 'https://rai-notflix.herokuapp.com/categorias';
 
 export default {
   URL_BACKEND_TOP,
